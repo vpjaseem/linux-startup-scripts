34e90d52602e1ac9318c5d2a521e541fc82062d5
vpjaseem <67306692+vpjaseem@users.noreply.github.com>
2020-07-10 02:01:54 +0530

    Update 2-clone-web-app.sh

--- 2-clone-web-app.sh
+++ 2-clone-web-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo git clone https://github.com/vpjaseem/sample-static-web-app.git /home/var/www/html/sample-static-web-app
-sudo cp -r /home/var/www/html/sample-static-web-app/* /$home/var/www/html/
+sudo git clone https://github.com/vpjaseem/sample-static-web-app.git
+#sudo cp -r /home/ec2-user/sample-static-web-app/* /$home/var/www/html/
 sudo service httpd restart
