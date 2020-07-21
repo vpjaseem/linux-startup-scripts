e4bc33b19dd405dfa802b99ce0688a40d871f275
vpjaseem <67306692+vpjaseem@users.noreply.github.com>
2020-07-21 08:47:38 +0530

    Update 3-clone-sample-php-web-app.sh

--- 3-clone-sample-php-web-app.sh
+++ 3-clone-sample-php-web-app.sh
@@ -2,4 +2,5 @@
 cd /$home/var/www/html/
 sudo git clone https://github.com/vpjaseem/sample-static-php-web-app.git
 sudo cp -r /var/www/html/sample-static-php-web-app/* /var/www/html/
+sudo rm -r sample-static-php-web-app
 sudo service httpd restart
